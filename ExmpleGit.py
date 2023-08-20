bd1a3c25c40aa73d7d2ed66cdab2abca2fc87f61
Owner <hadad.yanay@gmail.com>
2023-08-20 13:57:32 +0300

    commit initial

--- ExmpleGit.py
+++ ExmpleGit.py
@@ -2,3 +2,11 @@ print("Hello World")
 print("yanay hadad")
 print("hello yanay")
 print("thank you")
+while True:
+    print("please enter your name: ")
+    user_num = input()
+    if user_num != "yanay":
+        print("you didn't enter the manner name try again: ")
+        user_num = input()
+    else:
+        break
